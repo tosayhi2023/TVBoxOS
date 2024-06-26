10aeab470fbbd7a0c67a86db26cf933aac822bdd
tosayhi2023 <151717465+tosayhi2023@users.noreply.github.com>
2024-06-26 14:06:31 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://www.poctechcorp.com/upload/yuexi.json");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "http://www.poctechcorp.com/upload/myt.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
